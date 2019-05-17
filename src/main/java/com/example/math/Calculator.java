f86d742e1b16823d7e7ca1e46a4e3fcfae725fa8
sfagade <simonPeter124>
2019-05-17 13:13:47 +0100

    made minor change

--- src/main/java/com/example/math/Calculator.java
+++ src/main/java/com/example/math/Calculator.java
@@ -3,6 +3,7 @@ package com.example.math;
 public class Calculator {
 
     public int add(int a, int b) {
+        System.out.println("Making some comments for comments sake");
         return a + b;
     }
 
